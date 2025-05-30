23aae9ea65d981275a5c7cc73c7c3b4f0caa0b0a
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2025-05-29 22:44:22 -0300

    mudanças simples

--- script/module/menu.js
+++ script/module/menu.js
@@ -1,15 +1,15 @@
-
 export function initMenuHamburguer() {
-    const botao = document.getElementById('menuHamburguer');
-    const menu = document.getElementById('menu');
+    const menuIcone = document.getElementById("menuHamburguer");
+    const menu = document.getElementById("menu");
 
-    botao.addEventListener('click', () => {
-        menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    menuIcone.addEventListener("click", () => {
+        menu.classList.toggle("ativo");
     });
 
-    document.addEventListener('click', function (event) {
-        if (!botao.contains(event.target) && !menu.contains(event.target)) {
-        menu.style.display = 'none';
+    document.addEventListener("click", (event) => {
+        const clicouFora = !menu.contains(event.target) && !menuIcone.contains(event.target);
+        if (clicouFora) {
+            menu.classList.remove("ativo");
         }
     });
 }
